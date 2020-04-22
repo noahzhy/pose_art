57da89339070691bd88af7790e9147b839917a59
noahzhy <noahzhy@gmail.com>
2020-04-22 12:19:17 +0900

    first test passed

--- pose_test.py
+++ pose_test.py
@@ -1,11 +1,21 @@
+import pyrealsense2 as rs
+import numpy as np
+import platform
+import argparse
+import cv2
+import os
+
+from pprint import pprint
 from cubemos.core.nativewrapper import CM_TargetComputeDevice
 from cubemos.core.nativewrapper import initialise_logging, CM_LogLevel
 from cubemos.skeleton_tracking.nativewrapper import Api, SkeletonKeypoints
-import cv2
-import argparse
-import os
-import platform
-from pprint import pprint
+
+confidence_threshold = 0.4
+
+pipe = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.color, 320, 240, rs.format.bgr8, 30)
+pipe.start(config)
 
 keypoint_ids = [
     (1, 2),
@@ -86,81 +96,52 @@ def render_result(skeletons, img, confidence_threshold):
                 img, limb[0], limb[1], skeleton_color, thickness=2, lineType=cv2.LINE_AA
             )
 
-
-parser = argparse.ArgumentParser(description="Perform keypoing estimation on an image")
-parser.add_argument(
-    "-c",
-    "--confidence_threshold",
-    type=float,
-    default=0.5,
-    help="Minimum confidence (0-1) of displayed joints",
-)
-parser.add_argument(
-    "-v",
-    "--verbose",
-    action="store_true",
-    help="Increase output verbosity by enabling backend logging",
-)
-
-parser.add_argument(
-    "-o",
-    "--output_image",
-    type=str,
-    default="output/002.jpg",
-    help="filename of the output image",
-)
-
-parser.add_argument(
-    "--image",
-    metavar="I",
-    default="arts_res/002.jpg",
-    type=str,
-    help="filename of the input image"
-)
-
-
-
 # Main content begins
 if __name__ == "__main__":
     try:
-        #Parse command line arguments
-        args = parser.parse_args()
         check_license_and_variables_exist()
-        #Get the path of the native libraries and ressource files
+        # Get the path of the native libraries and ressource files
         sdk_path = os.environ["CUBEMOS_SKEL_SDK"]
-        if args.verbose:
-            initialise_logging(sdk_path, CM_LogLevel.CM_LL_DEBUG, True, default_log_dir())
 
-        img = cv2.imread(args.image)
-        #initialize the api with a valid license key in default_license_dir()
+        # initialize the api with a valid license key in default_license_dir()
         api = Api(default_license_dir())
         model_path = os.path.join(
-            sdk_path, "models", "skeleton-tracking", "fp32", "skeleton-tracking.cubemos"
+            sdk_path,
+            "models",
+            "skeleton-tracking",
+            "fp32",
+            "skeleton-tracking.cubemos"
         )
         api.load_model(CM_TargetComputeDevice.CM_CPU, model_path)
-        #perform inference
-        skeletons = api.estimate_keypoints(img, 192)
 
-        # perform inference again to demonstrate tracking functionality.
-        # usually you would estimate the keypoints on another image and then
-        # update the tracking id
-        new_skeletons = api.estimate_keypoints(img, 192)
-        new_skeletons = api.update_tracking_id(skeletons, new_skeletons)
+        while True:
+            frames = pipe.wait_for_frames()
+            color_frame = frames.get_color_frame()
+            color_image = np.asanyarray(color_frame.get_data())
 
-        render_result(skeletons, img, args.confidence_threshold)
-        print("Detected skeletons: ", len(skeletons))
-        if args.verbose:
-          print(skeletons)
-          
-        if args.output_image:
-            isSaved = cv2.imwrite(args.output_image, img)
-            if isSaved:
-                print("The result image is saved in: ", args.output_image)
-            else:
-                print("Saving the result image failed for the given path: ", args.output_image)
+            skeletons = api.estimate_keypoints(color_image, 192)
+            # perform inference again to demonstrate tracking functionality.
+            # usually you would estimate the keypoints on another image and then
+            # update the tracking id
+            new_skeletons = api.estimate_keypoints(color_image, 192)
+            new_skeletons = api.update_tracking_id(skeletons, new_skeletons)
+            # render the points
+            render_result(skeletons, color_image, confidence_threshold)
 
+            cv2.namedWindow("preview", cv2.WINDOW_AUTOSIZE)
+            cv2.imshow("preview", color_image)
+            
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q') or key == 27:
+                cv2.destroyAllWindows()
+                break
 
+        # print("Detected skeletons: ", len(skeletons))
             
     except Exception as ex:
         print("Exception occured: \"{}\"".format(ex))
-# Main content ends
+
+    finally:
+        pipe.stop()
+
+
