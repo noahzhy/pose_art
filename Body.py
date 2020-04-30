26a69fb69a7aa9a59ed6a98f15841563cfd2861d
noahzhy <noahzhy@gmail.com>
2020-04-30 17:09:01 +0900

    add

--- Body.py
+++ Body.py
@@ -55,15 +55,14 @@ class Evaluate:
         for i in evaluate_position:
             (pre, nex) = self.association_point(i)
             angle = self.angle(self.body[pre], self.body[i],self.body[nex])
-            print(angle)
+            # print(angle)
 
 
 if __name__ == "__main__":
     bodies = [SkeletonKeypoints(joints=[Coordinate(x=131.25, y=48.75), Coordinate(x=131.25, y=96.25), Coordinate(x=96.25, y=96.25), Coordinate(x=53.75, y=133.75), Coordinate(x=33.75, y=83.75), Coordinate(x=166.25, y=98.75), Coordinate(x=196.25, y=136.25), Coordinate(x=216.25, y=101.25), Coordinate(x=116.25, y=208.75), Coordinate(x=-1.0, y=-1.0), Coordinate(x=-1.0, y=-1.0), Coordinate(x=158.75, y=203.75), Coordinate(x=-1.0, y=-1.0), Coordinate(x=-1.0, y=-1.0), Coordinate(x=126.25, y=43.75), Coordinate(x=138.75, y=43.75), Coordinate(x=116.25, y=53.75), Coordinate(x=148.75, y=53.75)], confidences=[0.9249143600463867, 0.9447815418243408, 0.8818870186805725, 0.92684006690979, 0.8767169713973999, 0.8767545223236084, 0.8759602308273315, 0.8763948678970337, 0.5975141525268555, 0.0, 0.0, 0.6924083232879639, 0.0, 0.0, 1.0069090127944946, 0.9924660921096802, 0.9715789556503296, 0.9577101469039917], id=0)]
-
+    # bodies = open()
     for i in bodies:
         body = Body(i)
         eva = Evaluate(body)
-        # print(body.parser())
         eva.calculate_angles()
         # print(eva.calculate_angles())
---
+++ estimate_keypoints.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+from cubemos.core.nativewrapper import CM_TargetComputeDevice
+from cubemos.core.nativewrapper import initialise_logging, CM_LogLevel
+from cubemos.skeleton_tracking.nativewrapper import Api, SkeletonKeypoints
+import cv2
+import os
+import platform
+from pprint import pprint
+
+keypoint_ids = [
+    (1, 2),
+    (1, 5),
+    (2, 3),
+    (3, 4),
+    (5, 6),
+    (6, 7),
+    (1, 8),
+    (8, 9),
+    (9, 10),
+    (1, 11),
+    (11, 12),
+    (12, 13),
+    (1, 0),
+    (0, 14),
+    (14, 16),
+    (0, 15),
+    (15, 17),
+]
+
+
+def default_log_dir():
+    if platform.system() == "Windows":
+        return os.path.join(os.environ["LOCALAPPDATA"], "Cubemos", "SkeletonTracking", "logs")
+    elif platform.system() == "Linux":
+        return os.path.join(os.environ["HOME"], ".cubemos", "skeleton_tracking", "logs")
+    else:
+        raise Exception("{} is not supported".format(platform.system()))
+
+
+def default_license_dir():
+    if platform.system() == "Windows":
+        return os.path.join(os.environ["LOCALAPPDATA"], "Cubemos", "SkeletonTracking", "license")
+    elif platform.system() == "Linux":
+        return os.path.join(os.environ["HOME"], ".cubemos", "skeleton_tracking", "license")
+    else:
+        raise Exception("{} is not supported".format(platform.system()))
+
+
+def check_license_and_variables_exist():
+    license_path = os.path.join(default_license_dir(), "cubemos_license.json")
+    if not os.path.isfile(license_path):
+        raise Exception(
+            "The license file has not been found at location \"" +
+            default_license_dir() + "\". "
+            "use the post-installation script to generate the license file")
+    if "CUBEMOS_SKEL_SDK" not in os.environ:
+        raise Exception(
+            "The environment Variable \"CUBEMOS_SKEL_SDK\" is not set. "
+        )
+
+
+def get_valid_limbs(keypoint_ids, skeleton, confidence_threshold):
+    limbs = [
+        (tuple(map(int, skeleton.joints[i])), tuple(map(int, skeleton.joints[v])))
+        for (i, v) in keypoint_ids
+        if skeleton.confidences[i] >= confidence_threshold
+        and skeleton.confidences[v] >= confidence_threshold
+    ]
+    valid_limbs = [
+        limb
+        for limb in limbs
+        if limb[0][0] >= 0 and limb[0][1] >= 0 and limb[1][0] >= 0 and limb[1][1] >= 0
+    ]
+    return valid_limbs
+
+
+def render_result(skeletons, img, confidence_threshold):
+    skeleton_color = (100, 254, 213)
+    for index, skeleton in enumerate(skeletons):
+        limbs = get_valid_limbs(keypoint_ids, skeleton, confidence_threshold)
+        for limb in limbs:
+            cv2.line(img, limb[0], limb[1], skeleton_color, thickness=2, lineType=cv2.LINE_AA)
+
+
+class SKP:
+    def __init__(self):
+        super().__init__()
+        check_license_and_variables_exist()
+        self.confidence_threshold = 0.5
+        self.output_path = "output"
+        self.skp_output_path = "skp_output"
+        self.api = Api(default_license_dir())
+        self.model_path = os.path.join(
+            os.environ["CUBEMOS_SKEL_SDK"], "models", "skeleton-tracking", "fp32", "skeleton-tracking.cubemos"
+        )
+
+    def get_skp_from_pic(self, pic_path):
+        try:
+            img = cv2.imread(pic_path)
+            self.api.load_model(CM_TargetComputeDevice.CM_CPU, self.model_path)
+            skeletons = self.api.estimate_keypoints(img, 192)
+            render_result(skeletons, img, self.confidence_threshold)
+            file_name = os.path.basename(pic_path)
+            with open("{}/{}.txt".format(self.skp_output_path, os.path.splitext(file_name)[0]), "w") as f:
+                f.write(str(skeletons))
+
+            isSaved = cv2.imwrite("{}/{}".format(self.output_path, file_name), img)
+
+        except Exception as ex:
+            print("Exception occured: \"{}\"".format(ex))
+
+
+if __name__ == "__main__":
+    skp = SKP()
+    skp.get_skp_from_pic("arts_res/001.jpg")
